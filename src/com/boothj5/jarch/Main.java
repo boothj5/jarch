d3a668233b7b9e638f87b31939ac24406f6d0bee
James Booth <boothj5@gmail.com>
2012-06-03 02:31:35 +0100

    Filter imports

--- src/com/boothj5/jarch/Main.java
+++ src/com/boothj5/jarch/Main.java
@@ -1,7 +1,13 @@
 package com.boothj5.jarch;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -38,6 +44,18 @@ public class Main {
                 System.out.println("---------------------------");
                 for (File file : moduleFiles) {
                     System.out.println(file.getName());
+                    FileInputStream fstream = new FileInputStream(file);
+                    DataInputStream in = new DataInputStream(fstream);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+
+                    while ((strLine = br.readLine()) != null)   {
+                        if (strLine.startsWith("import " + basePackage))
+                        
+                        
+                        System.out.println ("Found " + strLine);
+                    }                   
+                    
                 }
             }
         }
