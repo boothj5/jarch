19d1494ea956a8dddf77caffeabfb5ef72a5e0ae
James Booth <boothj5@gmail.com>
2012-06-10 01:59:01 +0100

    More invalid tests

--- test/resources/invalid/com/boothj5/jarchexample/application/person/controller/PersonController.java
+++ test/resources/invalid/com/boothj5/jarchexample/application/person/controller/PersonController.java
@@ -4,3 +4,6 @@ import java.util.List;
 
 import com.boothj5.jarchexample.application.common.*;
 import com.boothj5.jarchexample.application.person.facade.PersonFacade;
+import com.boothj5.jarchexample.application.person.service.PersonService;
+import com.boothj5.jarchexample.application.person.repository.PersonRepository;
+import com.boothj5.jarchexample.application.person.dao.PersonDAO;
--- test/resources/invalid/com/boothj5/jarchexample/configuration/controller/ConfigurationController.java
+++ test/resources/invalid/com/boothj5/jarchexample/configuration/controller/ConfigurationController.java
@@ -4,3 +4,6 @@ import java.util.List;
 
 import com.boothj5.jarchexample.common.*;
 import com.boothj5.jarchexample.configuration.facade.ConfigurationFacade;
+import com.boothj5.jarchexample.configuration.service.ConfigurationService;
+import com.boothj5.jarchexample.configuration.repository.ConfigurationRepository;
+import com.boothj5.jarchexample.configuration.dao.ConfigurationDAO;
